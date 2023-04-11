da24725f4c78c4d646a4da53c50c2605314e71bb
Surya Prakash Chaudhary <67498076+Suryac72@users.noreply.github.com>
2023-04-11 22:45:37 +0530

    Update site-content.ts

--- components/data/site-content.ts
+++ components/data/site-content.ts
@@ -2,7 +2,7 @@ export const siteData = {
     about: {
         title: "About",
         subtitle: "Who I am",
-        innerSubTitle: "SubTitle",
+        innerSubTitle: "I am a Full-Stack Web Developer",
         description: " As a full-stack web developer, I have the skills and experience to build web applications from start to finish. I am proficient in both front-end and back-end development, and have a deep understanding of how these two components work together to create a seamless user experience. On the front-end, I am skilled in HTML, CSS, and JavaScript, as well as popular front-end frameworks such as React. I have a keen eye for design and am able to create visually appealing and intuitive user interfaces. On the back-end, I am experienced in server-side languages such as Node.js, Java and am familiar with popular databases such as MySQL, PostgreSQL, and MongoDB. I am able to architect scalable and secure systems that can handle large amounts of data and traffic. In addition to my technical skills, I am also an excellent communicator and collaborator. I am able to work effectively with designers, product managers, and other developers to ensure that projects are delivered on time and to the highest standard. Overall, I am passionate about creating high-quality web applications that provide real value to users. I am always learning and expanding my skills, and am excited to take on new challenges and projects.",
     },
 
