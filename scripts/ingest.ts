7e7c958c86ac7bfb84e7c31292d20bf873e8f35d
Surya Prakash Chaudhary <suryac72@gmail.com>
2025-12-19 08:53:15 +0530

    fix:variables

--- scripts/ingest.ts
+++ scripts/ingest.ts
@@ -15,7 +15,7 @@ dotenv.config();
 
 const sanity = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   useCdn: false,
   apiVersion: "2023-05-03",
 });
--- utils/sanity-client.ts
+++ utils/sanity-client.ts
@@ -3,7 +3,7 @@ import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2024-07-27",
   useCdn: true,
 });
